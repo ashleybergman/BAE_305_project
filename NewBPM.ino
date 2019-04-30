a2ff0a5925307ab39e8087e10689b7c9c86f0c6b
kkr244 <46691857+kkr244@users.noreply.github.com>
2019-04-30 11:44:19 -0400

    Add files via upload

---
+++ NewBPM.ino
@@ -0,0 +1,66 @@
+#include "arduinoFFT.h"
+
+arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+/*
+These values can be changed in order to evaluate the functions
+*/
+#define CHANNEL A0
+const uint16_t samples = 128; //This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 50; //Hz, must be less than 10000 due to ADC
+
+unsigned int sampling_period_us;
+unsigned long microseconds;
+
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+double vReal[samples];
+double vImag[samples];
+double maxfreq;
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
+void setup()
+{
+  sampling_period_us = round(1000000*(1.0/samplingFrequency));
+  Serial.begin(115200);
+  Serial.println("Ready");
+
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+
+}
+
+void loop()
+{
+  /*SAMPLING*/
+  for(int i=0; i<samples; i++)
+  {
+      microseconds = micros();    //Overflows after around 70 minutes!
+
+      vReal[i] = analogRead(CHANNEL);
+      Serial.print( vReal[i]);
+      Serial.print( " ");
+      Serial.println( maxfreq *60);
+      vImag[i] = 0;
+      while(micros() < (microseconds + sampling_period_us)){
+        //empty loop
+      }
+  }
+  /* Print the results of the sampling according to time */
+  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);  /* Weigh data */
+  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+  maxfreq = FFT.MajorPeak(vReal, samples, samplingFrequency);
+  
+  if ((maxfreq*60) <= 100 && (maxfreq*60) >= 57){
+      digitalWrite(6, HIGH);
+      digitalWrite(7,LOW);}
+      else{
+        digitalWrite(7, HIGH);
+        digitalWrite(6,LOW);
+      }
+}
